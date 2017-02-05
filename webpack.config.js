38d0c98005b62fe3de905291a22034727d8c803c
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-02-05 20:26:45 +0200

    css styling for home

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
       }
     ]
   }

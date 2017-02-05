84c03efbd93880851d0513d6b8ef1c66343f32db
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-02-05 14:06:38 +0200

    Added MongoLab heroku integration

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
-const mongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 1002
+const dbURI = process.env.PROD_MONGODB
+
+mongoose.Promise = global.Promise
+mongoose.connect(dbURI)
+
+const db = mongoose.connection
+console.log(dbURI)
+db.on('error', console.error.bind(console), 'connection error')
+db.once('open', () => {
+    console.log('db connected')
+})
 
 app.set('view engine', 'pug')
 

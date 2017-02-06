7951e5c83277ef46edab1d308a3c7f566d2e1983
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-02-06 15:30:49 +0200

    displayed entry picture attachment in pug

--- client/js/index.js
+++ client/js/index.js
@@ -5,6 +5,3 @@ let editableEls = document.querySelector('.editable')
 let editor = new MediumEditor(editableEls, {
     forcePlainText: false
 })
-
-preview.config({ prvBox:'preview' })
-           .listen('#uploader');
